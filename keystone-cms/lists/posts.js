1ca2b20e339604cfd5af82f86a1093ac4a03213e
Nick Hernandez <xnickhernandez@gmail.com>
2021-09-24 06:11:11 +0000

    adjusted project structure

---
+++ keystone-cms/lists/posts.js
@@ -0,0 +1,10 @@
+const { Text } = require('@keystonejs/fields');
+
+module.exports = {
+  fields: {
+    title: { type: Text, isRequired: true },
+    article: {type: Text, isRequired: true},
+    // image field
+    // date field
+  }
+}
