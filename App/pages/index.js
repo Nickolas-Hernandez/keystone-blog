9d8f3eca14074024892abe62d1341ab783d1b6c9
Nick Hernandez <xnickhernandez@gmail.com>
2021-10-08 03:21:57 +0000

    Set head and meta data for home page.

--- App/pages/index.js
+++ App/pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
+    <Head>
+      <title>The Dev Post</title>
+      <meta property="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <h1>Hello Mutha FFFUECKERsssss</h1>
   )
 }
