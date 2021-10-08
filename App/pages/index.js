eedc8bd97130a8377de406bbc595701674fc5822
Nick Hernandez <xnickhernandez@gmail.com>
2021-10-08 03:37:20 +0000

    Created header component

--- App/pages/index.js
+++ App/pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
+import Header from  '../components/header';
 
 
 export default function Home() {
   return (
+    <>
     <Head>
       <title>The Dev Post</title>
       <meta property="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <h1>Hello Mutha FFFUECKERsssss</h1>
+    <Header />
+    </>
   )
 }
