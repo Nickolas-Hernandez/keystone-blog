c07a2ce1d836d2c280fb12bfe1b46d08e8cfabc1
Nick Hernandez <xnickhernandez@gmail.com>
2021-10-13 06:13:07 +0000

    moved nav styles into a seperate file and imported them in _app.js.

--- App/pages/_app.js
+++ App/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import '../styles/type.css';
 import '../styles/layout.css';
 import '../styles/header.css';
+import '../styles/nav.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
