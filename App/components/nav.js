42234661fccb78291c5c210b4c472304e1c5b741
Nick Hernandez <xnickhernandez@gmail.com>
2021-10-15 05:48:58 +0000

    Imported various react hooks to handle nav animation

--- App/components/nav.js
+++ App/components/nav.js
@@ -1,13 +1,29 @@
+import React, { useState, useEffect, useRef } from 'react';
+
 function Nav({ categories }) {
   const tabs = categories.map(category => {
     return(
       <CategoryTab categoryName={category.categoryName} /> // add key
     );
   });
+
+  const [ xPosition, setXPosition] = useState(0);
+  const slider = useRef(null);
+  useEffect(() => {
+    const rect = slider.current.getBoundingClientRect();
+    console.log('span', rect.left);
+    setXPosition(rect.left);
+  })
+
+  const animationHandler = (event) => {
+    if(event.target.className !== 'nav-link') return;
+    console.log('tab', event.target.getBoundingClientRect());
+  };
+
   return (
-    <nav>
+    <nav onClick={animationHandler}>
       <ul className="nav-links">{tabs}</ul>
-      <span className="nav-slider"></span>
+      <span className="nav-slider" ref={slider}></span>
     </nav>
   );
 }
@@ -16,8 +32,13 @@ function CategoryTab ({ categoryName }){
   const url = `/${categoryName}`;
   const isActive = (categoryName === 'Dev') ? true : false;
   return (
-    <li className={isActive ? 'nav-link active' : 'nav-link'}><a href={url}>{categoryName}</a></li>
+    <li className={isActive ? 'nav-link active' : 'nav-link'}>{categoryName}</li>
   );
 }
+
+// function animateTab(event){
+//   if(event.target.className !== 'nav-link') return;
+//   console.log(event.target.getBoundingClientRect());
+// }
 
 export default Nav;
