6b068aba496ac81184e6c6f551d95e4d3298ba18
Nick Hernandez <xnickhernandez@gmail.com>
2021-10-15 06:16:27 +0000

    Added click event to shift postion of nav slider

--- App/components/nav.js
+++ App/components/nav.js
@@ -7,17 +7,21 @@ function Nav({ categories }) {
     );
   });
 
-  const [ xPosition, setXPosition] = useState(0);
+  // const [ xPosition, setXPosition] = useState(0);
+
   const slider = useRef(null);
-  useEffect(() => {
-    const rect = slider.current.getBoundingClientRect();
-    console.log('span', rect.left);
-    setXPosition(rect.left);
-  })
+  // useEffect(() => {
+  //   const rect = slider.current.getBoundingClientRect();
+  //   console.log('span', rect.left);
+  //   setXPosition(rect.left);
+  // })
 
   const animationHandler = (event) => {
     if(event.target.className !== 'nav-link') return;
-    console.log('tab', event.target.getBoundingClientRect());
+    const tabRect = event.target.getBoundingClientRect();
+    const nextPosition = tabRect.left;
+    slider.current.style.left = nextPosition + 'px';
+    console.log(slider.current.style);
   };
 
   return (
@@ -35,10 +39,5 @@ function CategoryTab ({ categoryName }){
     <li className={isActive ? 'nav-link active' : 'nav-link'}>{categoryName}</li>
   );
 }
-
-// function animateTab(event){
-//   if(event.target.className !== 'nav-link') return;
-//   console.log(event.target.getBoundingClientRect());
-// }
 
 export default Nav;
